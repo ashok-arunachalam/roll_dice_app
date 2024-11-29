38fd8516c3839dfa3f0b635c85f5a133b9f91351
Ashok A <ashoka@opentext.com>
2024-11-29 23:30:40 +0530

    Added test for the roll dice application

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,28 +1,31 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:roll_dice_app/dice_roller.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    //await tester.pumpWidget(const MyApp());
+  testWidgets('Dice image changes on button press', (WidgetTester tester) async {
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    //Arrange
+    // Build the widget
+    await tester.pumpWidget(const MaterialApp(home: DiceRoller()));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Find the button
+    final buttonFinder = find.byType(ElevatedButton);
+    expect(buttonFinder, findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    //Act
+    // Simulate a button press
+    await tester.tap(buttonFinder);
+
+    // Verify the dice image has changed
+    final oldDiceImage = find.byType(Image);
+
+    // Simulate a button press
+    await tester.tap(buttonFinder);
+
+    final newDiceImage = find.byType(Image);
+
+    //Assert
+    expect(oldDiceImage, isNot(newDiceImage));
   });
 }
